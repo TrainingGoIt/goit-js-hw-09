b4072ccedb1c2402db65550e3612e838164581c1
Victoria257 <110929199+Victoria257@users.noreply.github.com>
2023-01-21 21:19:55 +0000

    Deploying to gh-pages from @ Victoria257/goit-js-hw-09@0ce6c3c69458827c728505d4dc2f85e00190f0df 🚀

--- 03-promises.38a4b64a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form"),document.querySelector("button"),document.querySelector(".delay"),document.querySelector(".step"),document.querySelector(".amount");
-//# sourceMappingURL=03-promises.38a4b64a.js.map
--- 03-promises.fa8ff42f.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form"),document.querySelector("button"),document.querySelector(".delay"),document.querySelector(".step"),document.querySelector(".amount");
-//# sourceMappingURL=03-promises.fa8ff42f.js.map
