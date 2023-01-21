0ce6c3c69458827c728505d4dc2f85e00190f0df
Viktoria <vik25@i.ua>
2023-01-21 22:19:28 +0100

    add task3 (what i know)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,24 +1,63 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
 
 const form = document.querySelector(".form");
 const button = document.querySelector("button");
-const delay = document.querySelector(".delay");
-const step = document.querySelector(".step");
-const amount = document.querySelector(".amount")
+const delayForm = document.querySelector(".delay");
+const stepForm = document.querySelector(".step");
+const amountForm = document.querySelector(".amount")
+
+let position;
+let delay;
+let amount;
+  
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  delay = Number(delayForm.value);
+  amount = Number(amountForm.value);
+  step = Number(stepForm.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    position = i
+    createPromise(position, delay)
+    delay += step 
+  }
+   
+});
 
 
-// Напиши скрипт, який на момент сабміту форми викликає функцію
-// createPromise(position, delay) стільки разів, скільки ввели в
-// поле amount.Під час кожного виклику передай їй номер промісу(position),
-//   що створюється, і затримку, враховуючи першу затримку(delay), введену
-// користувачем, і крок(step).
+function createPromise(position, delay) {
+  console.log(position)
+  console.log(delay);
+  const promise = new Promise((resolve) => {
+    setInterval(() => {
+    // resolve({"position" : position, "delay": delay})
+      resolve({ position, delay })
+    })
+  }, delay)
+
+//  const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+
+
+  promise.then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+};
+
+
+
+
+//!----------------------------------------------
 
 // Доповни код функції createPromise таким чином,
 //   щоб вона повертала один проміс, який виконується або
